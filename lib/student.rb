ebabd5226e874c246fed502961ecfadd64d3af5a
Matt Farmer <mrfarmer777@gmail.com>
2018-06-11 02:03:17 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -53,7 +53,7 @@ class Student
 
   def self.first_student_in_grade_10
     sql="SELECT * FROM students WHERE grade=10 LIMIT 1"
-    stu=DB[:conn].execute(sql).flatten.first
+    stu=DB[:conn].execute(sql)[0][0]
     Student.new_from_db(stu)
   end
 
